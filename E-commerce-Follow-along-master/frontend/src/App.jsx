f8125bf6c499a14011e26a8476539067f6f48c0f
bhavendra <bhavendrakumar007@gmail.com>
2025-02-20 11:19:50 +0530

    donee

--- E-commerce-Follow-along-master/frontend/src/App.jsx
+++ E-commerce-Follow-along-master/frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Routes,Route} from 'react-router-dom'
-import { LoginPage, CreateProduct } from './Routes/Routes'
+import { LoginPage,signuppage,CreateProduct } from './Routes/Routes'
 function App() {
   
 
