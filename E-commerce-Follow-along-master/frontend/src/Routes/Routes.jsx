88c236dcefe3a38bea15fcc8c6469eb4bc3ea927
bhavendra <bhavendrakumar007@gmail.com>
2025-02-28 14:14:45 +0530

    done

--- E-commerce-Follow-along-master/frontend/src/Routes/Routes.jsx
+++ E-commerce-Follow-along-master/frontend/src/Routes/Routes.jsx
@@ -1,5 +1,8 @@
-import LoginPage from "../Pages/loginPage/LoginPage";
-import CreateProduct from "../Components/createproduct";
-export {LoginPage, CreateProduct}
+// routes/Routes.jsx
+import LoginPage from './pages/Login'; // Correct path to Login.jsx
+import SignupPage from './pages/Signup';
+import ActivationPage from './pages/Activation'; // Correct path to Activation.jsx
+import HomePage from './pages/HomePage';
+import CreateProduct from '../pages/CreateProduct'
 
-
+export { LoginPage,SignupPage,ActivationPage, HomePage,CreateProduct   }; // Named export
